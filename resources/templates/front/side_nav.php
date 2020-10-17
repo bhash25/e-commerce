d2f43959213003c31c04dad36fe05ee5d19bf41d
bhash25 <62698649+bhash25@users.noreply.github.com>
2020-10-17 20:58:43 +0530

    Add files via upload

---
+++ resources/templates/front/side_nav.php
@@ -0,0 +1,21 @@
+<div class="col-md-3">
+                <p class="lead">Shop Name</p>
+                <div class="list-group">
+
+                <?php 
+
+                	$query = "SELECT * FROM category";
+                	$send_query = mysqli_query($conn, $query);
+
+                	if(!$send_query){
+                		die("QUERY FAILED".mysqli_error($conn));
+                	}
+
+                	while($row = mysqli_fetch_array($send_query)){
+                		echo "<a href='#'' class='list-group-item'>{$row['cat_title']}</a>";
+                	}
+
+                ?>
+
+                </div>
+            </div>
